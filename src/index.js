3f7e6b66e197628f21fa0c9311b5a0c04a92aaf0
Rafaela Lima Nogueira <rn-15@hotmail.com>
2020-07-28 17:35:34 -0300

    React router dom, criando rotas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Home from './Pages/Home';
+
+import CadastroVideo from './Pages/Cadastro/Video';
+
 import './index.css';
-import App from './App';
+
+
+const paginaDeErro404 = () => (
+  <div>
+    Error 404! Page Not Found.
+  </div>
+);
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      
+      <Route path="/" exact component={ Home } />
+      <Route path="/cadastro/video" component= {CadastroVideo } />
+
+
+
+      <Route component= { paginaDeErro404 } />
+
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
