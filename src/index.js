f150642820dcf828bddd5d73b432ec2952935828
Rafaela Lima Nogueira <rn-15@hotmail.com>
2020-07-29 13:09:42 -0300

    pág de erro personalizada

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 
 import Home from './pages/Home';
 import CadastroVideo from './pages/Cadastro/Video';
 import CadastroCategoria from './pages/Cadastro/Categoria';
 
+import Error from './assets/img/error.png';
+
+
+
 import './index.css';
 
 
 const paginaDeErro404 = () => (
-  <div>
-    Error 404! Page Not Found.
+  <div className="Erro">
+    
+    <img width="40%" src={Error} alt="Octocat Erro"/>
+    <h3>Ixii... Não encontramos a página!</h3>
+    <Link to="/">
+      Voltar para Home
+    </Link>
   </div>
 );
 
